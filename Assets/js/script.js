7c8d21135bd420c59d3ff271ac79962dbcbda838
Andrew <wing_andrew@hotmail.com>
2021-04-18 13:01:21 -0400

    Adding past, present, future styling to timeblocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,34 @@
+var timeBlocks = $('.time-block');
+var save = $('.saveBtn');
+
+// Get current time and run current time checker to update styles of timeblocks
+const currentTime = moment().hour();
+checkCurrentTime();
+
+// Display current date --------------------------------------------------------
 const today = moment().format("dddd, MMMM Do YYYY");
-console.log(today);
-
-
 const displayDate = document.getElementById("currentDate");
 
 displayDate.innerText = `Date: ${today}`;
+
+
+// Function to change row class based on current time (past, present, future) --
+
+function checkCurrentTime() {
+    timeBlocks.each(function () {
+    var currentBlock = parseInt($(this).attr('id').split('-')[1]);
+        if (currentBlock === currentTime) {
+            $(this).addClass('present');
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+        } else if (currentBlock < currentTime) {
+            $(this).addClass('past');
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+        } else {
+            $(this).addClass('future');
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+        };
+    });
+};
