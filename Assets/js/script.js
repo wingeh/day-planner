c297fd5ca94199ecd5d0d2cc191125383d21c644
Andrew <wing_andrew@hotmail.com>
2021-04-19 21:06:40 -0400

    Adding save to local storage feature

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -32,3 +32,23 @@ function checkCurrentTime() {
         };
     });
 };
+
+// Save to local storage ------------------------------------------------------ 
+
+$(document).ready(function () { 
+    save.on('click', function () {
+      var hours = $(this).siblings('.description').val();
+      var tasks = $(this).parent().attr('id');
+      localStorage.setItem(tasks, hours);
+    })
+});
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
